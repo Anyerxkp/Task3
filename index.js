ce0ea764e46d614c71e0b0705c6821965a078dda
Anyer <90461487+Anyerxkp@users.noreply.github.com>
2023-04-17 15:27:18 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,188 @@
+const redis = require('redis');
+const cache = require('express-redis-cache')();
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+const { check, validationResult } = require('express-validator');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/todo', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const todoSchema = new mongoose.Schema({
+  _id: { type: String, default: shortid.generate },
+  title: String,
+  description: String,
+  completed: Boolean
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+const redisClient = redis.createClient({
+  host: '127.0.0.1',
+  port: 6379
+});
+
+redisClient.on('error', (err) => {
+  console.log('Redis error:', err);
+});
+
+
+//Create a User schema and model:
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  role: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+//Add a signup route:
+
+app.post('/signup', async (req, res) => {
+  const { username, password, role } = req.body;
+  const existingUser = await User.findOne({ username });
+
+  if (existingUser) {
+    return res.status(400).json({ error: 'User already exists' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS));
+  const newUser = new User({ username, password: hashedPassword, role });
+
+  try {
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//Add a login route:
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+
+  res.status(200).json({ token });
+});
+
+//middlewater function
+
+const authMiddleware = (requiredRole) => (req, res, next) => {
+  const token = req.header('Authorization');
+
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied, no token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (requiredRole && decoded.role !== requiredRole) {
+      return res.status(403).json({ error: 'Forbidden, insufficient permissions' });
+    }
+    
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+
+// GET all todos
+app.get('/todos', authMiddleware(), cache.route(), async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// POST a new todo with validation
+app.post('/todos', authMiddleware('admin'), [
+  check('title').notEmpty().withMessage('Title is required'),
+  check('description').notEmpty().withMessage('Description is required')
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { title, description } = req.body;
+  const newTodo = new Todo({ title, description, completed: false });
+
+  try {
+    await newTodo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// PUT (update) a todo by ID with validation
+app.put('/todos/:id', authMiddleware('admin'), [
+  check('title').notEmpty().withMessage('Title is required'),
+  check('description').notEmpty().withMessage('Description is required'),
+  check('completed').isBoolean().withMessage('Completed must be a boolean value')
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { id } = req.params;
+  const { title, description, completed } = req.body;
+
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(id, { title, description, completed }, { new: true });
+    if (!updatedTodo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+    res.json(updatedTodo);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// DELETE a todo by ID
+app.delete('/todos/:id', authMiddleware('admin'), async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedTodo = await Todo.findByIdAndDelete(id);
+    if (!deletedTodo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+// At the end of your index.js file, add the following line:
+module.exports = app;
