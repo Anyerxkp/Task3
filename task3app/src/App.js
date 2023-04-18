490f7e0dea069b6aac4ad2362a795db4b5149c0d
Anyer <90461487+Anyerxkp@users.noreply.github.com>
2023-04-18 14:52:17 +0800

    Update App.js

--- task3app/src/App.js
+++ task3app/src/App.js
@@ -9,6 +9,7 @@ import {
   Form,
   ListGroup,
   Alert,
+  FormSelect,
 } from "react-bootstrap";
 import TodoItem from "./components/TodoItem";
 
@@ -140,123 +141,136 @@ const App = () => {
     <Col>
     <h1>Login</h1>
     <Form onSubmit={handleLogin}>
-    <Form.Group>
-    <Form.Label>Username</Form.Label>
-    <Form.Control
-    type="text"
-    value={loginData.username}
-    onChange={(e) =>
-    setLoginData({ ...loginData, username: e.target.value })
-    }
-    required
-    />
-    </Form.Group>
-    <Form.Group>
-    <Form.Label>Password</Form.Label>
-    <Form.Control
-    type="password"
-    value={loginData.password}
-    onChange={(e) =>
-    setLoginData({ ...loginData, password: e.target.value })
-    }
-    required
-    />
-    </Form.Group>
-    <Button type="submit">Login</Button>
-    </Form>
-    </Col>
-    <Col>
-    <h1>Signup</h1>
-    <Form onSubmit={handleSignup}>
-    <Form.Group>
-    <Form.Label>Username</Form.Label>
-    <Form.Control
-    type="text"
-    value={signupData.username}
-    onChange={(e) =>
-    setSignupData({ ...signupData, username: e.target.value })
-    }
-    required
-    />
-    </Form.Group>
-    <Form.Group>
-    <Form.Label>Password</Form.Label>
-    <Form.Control
-    type="password"
-    value={signupData.password}
-    onChange={(e) =>
-    setSignupData({ ...signupData, password: e.target.value })
-    }
-    required
-    />
-    </Form.Group>
-    <Button type="submit">Signup</Button>
-    </Form>
-    </Col>
-    </Row>
-    </>
-    ) : (
-    <>
-    <Row className="mt-4">
-    <Col>
-    <h1>Todo List</h1>
-    </Col>
-    <Col className="text-right">
-    <Button onClick={handleLogout}>Logout</Button>
-    </Col>
-    </Row>
-    <Row>
-    <Col>
-    {error && (
-    <Alert
-    variant="danger"
-    onClose={() => setError(null)}
-    dismissible
-    >
-    {error}
-    </Alert>
-    )}
-    <Form onSubmit={addTodo}>
-    <Form.Group>
-    <Form.Label>Title</Form.Label>
-    <Form.Control
-    type="text"
-    value={title}
-    onChange={(e) => setTitle(e.target.value)}
-    required
-    />
-    </Form.Group>
-    <Form.Group>
-    <Form.Label>Description</Form.Label>
-    <Form.Control
-    type="text"
-    value={description}
-    onChange={(e) => setDescription(e.target.value)}
-    required
-    />
-    </Form.Group>
-    <Button type="submit">Add Todo</Button>
-    </Form>
-    </Col>
-    </Row>
-    <Row>
-    <Col>
-    <ListGroup>
-    {todos.map((todo) => (
-    <TodoItem
-                     key={todo._id}
-                     todo={todo}
-                     onDelete={deleteTodo}
-                     onUpdate={updateTodo}
-                   />
-    ))}
-    </ListGroup>
-    </Col>
-    </Row>
-    </>
-    )}
+                <Form.Group>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={loginData.username}
+                    onChange={(e) =>
+                      setLoginData({ ...loginData, username: e.target.value })
+                    }
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    value={loginData.password}
+                    onChange={(e) =>
+                      setLoginData({ ...loginData, password: e.target.value })
+                    }
+                    required
+                  />
+                </Form.Group>
+                <Button type="submit">Login</Button>
+              </Form>
+            </Col>
+            <Col>
+              <h1>Signup</h1>
+              <Form onSubmit={handleSignup}>
+                <Form.Group>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={signupData.username}
+                    onChange={(e) =>
+                      setSignupData({ ...signupData, username: e.target.value })
+                    }
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    value={signupData.password}
+                    onChange={(e) =>
+                      setSignupData({ ...signupData, password: e.target.value })
+                    }
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Role</Form.Label>
+                  <Form.Select
+                    value={signupData.role}
+                    onChange={(e) =>
+                      setSignupData({ ...signupData, role: e.target.value })
+                    }
+                    required
+                  >
+                    <option value="admin">Admin</option>
+                    <option value="user">User</option>
+                  </Form.Select>
+                </Form.Group>
+                <Button type="submit">Signup</Button>
+              </Form>
+            </Col>
+          </Row>
+        </>
+      ) : (
+        <>
+          <Row className="mt-4">
+            <Col>
+              <h1>Todo List</h1>
+            </Col>
+            <Col className="text-right">
+              <Button onClick={handleLogout}>Logout</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              {error && (
+                <Alert
+                  variant="danger"
+                  onClose={() => setError(null)}
+                  dismissible
+                >
+                  {error}
+                </Alert>
+              )}
+              <Form onSubmit={addTodo}>
+                <Form.Group>
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+                <Button type="submit">Add Todo</Button>
+              </Form>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <ListGroup>
+                {todos.map((todo) => (
+                  <TodoItem
+                    key={todo._id}
+                    todo={todo}
+                    onDelete={deleteTodo}
+                    onUpdate={updateTodo}
+                  />
+                ))}
+              </ListGroup>
+            </Col>
+          </Row>
+        </>
+      )}
     </Container>
-    );
-    };
-    
-    export default App;
+  );
+};
+
+export default App;
